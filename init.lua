9230f498932d79d05bbb3594d80283ecca4e061d
Nick <rozhnik22@gmail.com>
2026-09-07 02:38:30 +0300

    Restore LSP servers

--- init.lua
+++ init.lua
@@ -740,8 +740,9 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
-    -- clangd = {},
-    -- gopls = {},
+    clangd = {},
+    gopls = {},
+    pylsp = {},
     -- pyright = {},
     -- tsc = {},
     --
